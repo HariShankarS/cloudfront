d17cec62910ee26783f6e314cfa0aee6e579f9c3
HariShankarS <s.harishankar333@gmail.com>
2024-07-22 00:09:10 +0530

    Add CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.asset_host = "https://d2br4o4vn0irzd.cloudfront.net"
+  config.asset_host = "d2br4o4vn0irzd.cloudfront.net"
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
